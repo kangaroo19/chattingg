c8aa22bd3b0f8b34f5d2b6642ca2c1423622950b
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-07-24 17:30:35 +0900

    123

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,23 @@ const mainWindow=document.querySelector("#main-window")
 const char=document.querySelector(".char")
 const chattingWindow=document.querySelector("#chatting-window")
 const firstchar=document.querySelector("#first-char")
+const secondchar=document.querySelector("#second-char")
+const firstcharImg=firstchar.firstChild.nextSibling
+
+const first={
+    img:firstchar.firstChild.nextSibling,
+    name:firstchar.firstChild.nextSibling.nextSibling
+}
+const second={
+    img:secondchar.firstChild.nextSibling,
+    name:secondchar.firstChild.nextSibling.nextSibling
+}
+
 let img=["주황버섯.gif","파란버섯.gif"]
 const array=['../img/스포아.png','../img/빨간달팽이.png','../img/슬라임.png','../img/리본돼지.png','../img/주황버섯.png','../img/초록버섯.png','../img/파란버섯.png','../img/뿔버섯.png']
+
+let firstime=0
+
 
 const body=document.querySelector("body");
 function charClick(e){ //클릭한 아이디값 가져오는 함수
@@ -14,8 +29,6 @@ function charClick(e){ //클릭한 아이디값 가져오는 함수
     let charId=e.getAttribute('id')
     let number=charId.substring(4,charId.length)
     let charName=array[number-1].substring(7,array[number-1].length-4)
-    console.log(number)
-    console.log(e)
     nameInput.value=charName
     startButton.addEventListener("click",()=>{
         mainWindow.classList.add("none")
@@ -25,17 +38,37 @@ function charClick(e){ //클릭한 아이디값 가져오는 함수
         let minutes=today.getMinutes()
         let milliseconds=today.getMilliseconds()
         let time=hours+minutes+milliseconds
-
+        firstime=time
         const param={
                     name:nameInput.value,
-                    img:'url('+array[number-1]+')'
+                    //img:'url('+array[number-1]+')'
+                    img:array[number-1]
         }
         
         socket.emit("chatting",param)
     })
 
+
     socket.on("chatting",(data)=>{
-        firstchar.style.backgroundImage=data.img
+        
+        
+        
+        if(first.img.childNodes.length===0){
+            const charImg1=document.createElement('img')
+            first.img.appendChild(charImg1)
+            charImg1.src=data.img
+            
+        }
+        else if(first.img.childNodes.length!==0){
+            const charImg2=document.createElement('img')
+            second.img.appendChild(charImg2)
+            charImg2.src=data.img
+        }
+        //charImg1.src=array[number-1]
+        // charImg.style.width='87px'
+        // charImg.style.height='75px'
+        // charImg.style.marginTop='15px'
+        
     })
 }
 //이미지반복 안되게
