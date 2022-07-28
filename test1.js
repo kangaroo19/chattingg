80eef62124106ffdd6ac4a1fca410f0aeec8a077
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-07-28 16:49:02 +0900

    123

--- test1.js
+++
@@ -1,8 +0,0 @@
-const body=document.querySelector("body");
-const images=["스포아.png"];
-
-const chosenImage=images[Math.floor(Math.random()*images.length)];
-
-console.log(chosenImage);
-
-body.style.backgroundImage='url('+chosenImage+')';
