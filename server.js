2be68454cd6cbc68e50177a425eb6758f1f22570
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-07-30 18:06:00 +0900

    123

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const WebSocket=require('ws')
 const ws=new WebSocket.Server({port:8004})
 
 let user_id=0
-let All_WS=[]
+let All_WS=[] //객체가 아닌 배열형태임에 유의
 ws.on('connection',function connect(websocket,req){ //startbutton 클릭시 연결됨
     user_id++
-    console.log('new user entered')
     sendUserID(user_id)
-    All_WS.push({'ws':websocket,'user_id':user_id,'user_name':''})
+    All_WS.push({'ws':websocket,'user_id':user_id,'user_name':null,'user_img':null})
+    
     websocket.on('message',(message)=>{
         message=JSON.parse(message)
         switch(message.code){
@@ -16,24 +16,22 @@ ws.on('connection',function connect(websocket,req){ //startbutton 클릭시 연�
                 All_WS.forEach((element,index)=>{
                     if(element.user_id==message.user_id){
                         element.user_name=message.name
+                        element.user_img=message.img
                     }
                 })
                 sendAllUsers()
                 break
             }
-            console.log(All_WS)
+            
     })
-    
     function sendUserID(user_id){
         let data={'code':'my_user_id','msg':user_id}
         websocket.send(JSON.stringify(data))
     }
     function sendAllUsers(){
-        let data={'code':'all_user_id','msg':JSON.stringify(All_WS)}
+        let data={'code':'all_users','msg':JSON.stringify(All_WS)}
         All_WS.forEach((element,index)=>{
-            console.log(data.msg.user_name)
             element.ws.send(JSON.stringify(data))
         })
     }
 })
-//sendalluser까지 구현
