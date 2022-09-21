ee7f09f8b2a69ed63d6a09b98ead622542c029cd
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-09-21 23:36:11 +0900

    123

--- server.js
+++ server.js
@@ -1,50 +1,3 @@
-//ngrok http 5005
-// const WebSocket=require('ws')
-// const ws=new WebSocket.Server({port:9000})
-
-// let user_id=0
-// let All_WS=[] //객체가 아닌 배열형태임에 유의
-
-// ws.on('connection',function connect(websocket,req){ //startbutton 클릭시 연결됨
-//     console.log('new user')
-//     user_id++
-//     sendUserID(user_id)
-//     All_WS.push({'ws':websocket,'user_id':user_id,'user_name':null,'user_img':null})
-    
-//     websocket.on('message',(message)=>{
-//         message=JSON.parse(message)
-//         switch(message.code){
-//             case 'connected_user':
-//                 //console.log(message)
-//                 All_WS.forEach((element,index)=>{
-//                     if(element.user_id==message.user_id){
-//                         element.user_name=message.name
-//                         element.user_img=message.img
-//                     }
-//                 })
-//                 sendAllUsers()
-//                 break
-//             case 'send_message':
-//                 All_WS.forEach((element,index)=>{
-//                     let data={'code':'chat_message','msg':message.msg,'sender_name':message.name}
-//                     element.ws.send(JSON.stringify(data))
-//                 })
-//                 break;
-//             }
-            
-//     })
-//     function sendUserID(user_id){
-//         let data={'code':'my_user_id','msg':user_id}
-//         websocket.send(JSON.stringify(data))
-//     }
-//     function sendAllUsers(){
-//         let data={'code':'all_users','msg':JSON.stringify(All_WS)}
-//         All_WS.forEach((element,index)=>{
-//             element.ws.send(JSON.stringify(data))
-//         })
-//     }
-// })
-
 
 const { count } = require("console")
 const express = require("express") 
@@ -111,7 +64,6 @@ io.on("connection",function connect(socket,req){
             }
             io.emit('p1',data)
             if(ALL_US[0].authority===true && ALL_US[1].authority===true){
-                //ALL_US[0].turn=true
                 io.emit('start',ALL_US)
             }
             io.emit('c1',ALL_US)
@@ -188,7 +140,7 @@ io.on("connection",function connect(socket,req){
         socket.emit('aaa',data)
     }
 })
-function lottoNum(){
+function lottoNum(){ 
     let lotto=[]
     let i=0;
     while(i<30){
@@ -204,11 +156,6 @@ function lottoNum(){
     return lotto
 }
 
-//리본돼지 뿔버섯 순으로 들어왔을때 
-//리본돼지 -> userid:1
-//뿔버섯 -> userid:2 인데
-//뿔버섯 레디버튼 누르고 리본돼지 레디버튼 누르면 잘 안됨
-//myid부분이 잘못된듯
-//client 부분에서 all_us 활용해볼것
+
 
 
--- test.js
+++
@@ -1,7 +0,0 @@
-for(let i=1;i<=30;i++){
-    $('.window1').append(`<img src="hidden-card.png" id=card${i} class=card>`)
-    if(i%5===0){
-        $('.window1').append(`<br>`)
-    }
-}
-$("#card1").attr("src", '뿔버섯.png');
